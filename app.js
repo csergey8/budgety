83fe88e2d0ae0dd22997b6d1ae873c922f9b39b3
csergey8 <c.sergey@protonmail.com>
2017-09-05 12:22:18 +0300

    read input data

--- app.js
+++ app.js
@@ -6,25 +6,45 @@ var budgetController = (function () {
 
 var UIController = (function () {
 
-  return {
-    getInput: function(){
-
-    }
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
   }
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, //get inc or exp
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+
+    },
+
+    getDOMStrings: function () {
+      return DOMStrings;
+    }
+  };
 
 })();
 
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMStrings();
+
   var ctrlAddItem = function () {
+    var input = UICtrl.getInput();
+    console.log(input);
 
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
   document.addEventListener('keypress', function (event) {
 
-    if (event.keyCode === 13 || event.which) {
+    if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
     }
   });
