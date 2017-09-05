bf343253c2777af8e78b65755a6abb6e0af58167
csergey8 <c.sergey@protonmail.com>
2017-09-05 11:29:25 +0300

    add keypress event

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 var budgetController = (function () {
 
-  var x = 23;
 
-  var add = function (a) {
-    return a + x;
-  }
 
-  return {
-    publicTest: function (b) {
-      return(add(b)); //
-    } ///
-  } //
 })();
 
 var UIController = (function () {
+
+  return {
+    getInput: function(){
+
+    }
+  }
 
 })();
 
 
 var controller = (function (budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
+  var ctrlAddItem = function () {
+
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.addEventListener('keypress', function (event) {
+
+    if (event.keyCode === 13 || event.which) {
+      ctrlAddItem();
+    }
+  });
 
 })(budgetController, UIController);
